279d3db3d47bbe9b9aa30da6a60b83a39d8eed46
ReapeR312 <vadim-matveev-01@mail.ru>
2025-09-20 22:00:43 +0300

    first commit

--- php/tgsend.php
+++ php/tgsend.php
@@ -1,23 +1,29 @@
 <?php
-$token = "7849389193:AAGTWxSJrAoNORFzHNekodsC70XheR61Abc";
-$chat_id = "541380619";
+$token = "";
+$chat_id = "";
 
 if ($_POST['act'] == 'order') {
     $name = $_POST['name'];
 	$phone = $_POST['phone'];
 	$message = urlencode("Новая заявка:\nИмя: $name\nТелефон: $phone");
-	
+
 	$url = "https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_id&text=$message";
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    $response = curl_exec($ch);
-    curl_close($ch);
-    
-    if ($response) {
-        echo "Заявка отправлена!";
-    } else {
-        echo "Ошибка при отправке";
-    }
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	$response = curl_exec($ch);
+
+	if ($response === false) {
+		echo 'Ошибка: ' . curl_error($ch);
+	} else if ($token == ""|| $chat_id == "") {
+		echo "Проверьте данные (надо поправить php файл. Указать токен и id чата)";
+	}else {
+		$back = $_SERVER['HTTP_REFERER'] ?? '/'; 
+        header("Location: $back");
+        exit;
+	}
+
+	curl_close($ch);
 }
 ?>
